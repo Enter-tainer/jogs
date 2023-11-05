8d19959abc77d44c4be7376e720421a2f57220dc
mgt <mgt@oi-wiki.org>
2023-11-05 17:47:04 +0800

    initial working version

---
+++ jogs.typ
@@ -0,0 +1,4 @@
+#let myplugin = plugin("jogs - stubbed.wasm")
+
+#str(myplugin.main(bytes("[1,2,3,4]+4")))
+
---
+++ src/lib.rs
@@ -0,0 +1,17 @@
+use quickjs_wasm_rs::JSContextRef;
+use wasm_minimal_protocol::*;
+
+initiate_protocol!();
+
+#[wasm_func]
+fn main(input: &[u8]) -> Result<Vec<u8>, String> {
+    let context = JSContextRef::default();
+    let Ok(input) = std::str::from_utf8(input) else {
+        return Err("input is not utf8".to_string());
+    };
+    let res = match context.eval_global("test.js", input) {
+        Ok(res) => res,
+        Err(err) => return Err(err.to_string()),
+    };
+    Ok(res.to_string().into_bytes())
+}
