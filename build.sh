1c28270f7e16a616eccae872b0a8860af8dc863e
mgt <mgt@oi-wiki.org>
2023-11-06 22:28:44 +0800

    Add set -euxo pipefail and stub with return 0

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+set -euxo pipefail
 cargo build --release --target wasm32-wasi
-wasi-stub ./target/wasm32-wasi/release/jogs.wasm -o typst-package/jogs.wasm
-wasm-opt typst-package/jogs.wasm -Oz --enable-bulk-memory -o typst-package/jogs.wasm
+wasi-stub -r 0 ./target/wasm32-wasi/release/jogs.wasm -o typst-package/jogs.wasm
+wasm-opt typst-package/jogs.wasm -O3 --enable-bulk-memory -o typst-package/jogs.wasm
