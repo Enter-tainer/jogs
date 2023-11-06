e7c64059657e0632b3177b9d4828acb1dbcffd25
mgt <mgt@oi-wiki.org>
2023-11-06 22:29:19 +0800

    Add serde_cbor dependency and update eval function to return CBOR-encoded result

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use quickjs_wasm_rs::JSContextRef;
+use quickjs_wasm_rs::{JSContextRef, from_qjs_value};
 use wasm_minimal_protocol::*;
 
 initiate_protocol!();
@@ -9,9 +9,15 @@ fn eval(input: &[u8]) -> Result<Vec<u8>, String> {
     let Ok(input) = std::str::from_utf8(input) else {
         return Err("input is not utf8".to_string());
     };
-    let res = match context.eval_global("test.js", input) {
-        Ok(res) => res,
+    let res = match context.eval_global("<evalScript>", input) {
+        Ok(res) => match from_qjs_value(res) {
+            Ok(res) => res,
+            Err(err) => return Err(err.to_string()),
+        },
         Err(err) => return Err(err.to_string()),
     };
-    Ok(res.to_string().into_bytes())
+    match serde_cbor::to_vec(&res) {
+        Ok(res) => Ok(res),
+        Err(err) => Err(err.to_string()),
+    }
 }
