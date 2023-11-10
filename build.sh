0e299288886da801c7590e6a0cb31f8609de4e37
mgt <mgt@oi-wiki.org>
2023-11-10 20:59:34 +0800

    Add license.html

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -euxo pipefail
 cargo build --release --target wasm32-wasi
+cargo about generate about.hbs > license.html
+cp license.html typst-package/
 wasi-stub -r 0 ./target/wasm32-wasi/release/jogs.wasm -o typst-package/jogs.wasm
 wasm-opt typst-package/jogs.wasm -O3 --enable-bulk-memory -o typst-package/jogs.wasm
