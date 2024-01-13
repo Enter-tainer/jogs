bbb80c59433e066a0ad697db80cb1915d0b97407
mgt <mgt@oi-wiki.org>
2024-01-13 13:46:56 +0800

    bump to v0.2.2

--- build.sh
+++ build.sh
@@ -3,5 +3,6 @@ set -euxo pipefail
 cargo build --release --target wasm32-wasi
 cargo about generate about.hbs > license.html
 cp license.html typst-package/
+cp README.md typst-package/
 wasi-stub -r 0 ./target/wasm32-wasi/release/jogs.wasm -o typst-package/jogs.wasm
 wasm-opt typst-package/jogs.wasm -O3 --enable-bulk-memory -o typst-package/jogs.wasm
